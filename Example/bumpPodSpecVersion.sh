7a57faf8ed373a6587c7fbf4eb593d85b0fcf0cd
taizi <changqin@ixiaopu.com>
2016-10-09 18:13:48 +0800

    bump

--- Example/bumpPodSpecVersion.sh
+++ Example/bumpPodSpecVersion.sh
@@ -6,5 +6,8 @@
 #  Created by taizi on 16/10/9.
 #  Copyright © 2016年 taizi. All rights reserved.
 
-podspec-bump -p ../ -w
-
+podspec-bump -p ../QGGImagePicker.podspec -w
+git commit -am "bump `podspec-bump --dump-version`"
+git tag "`podspec-bump --dump-version`"
+git push --tags
+pod trunk push
